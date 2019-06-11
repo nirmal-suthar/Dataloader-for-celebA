8a17bc0344193ae76a2fc5d5c90cbc64f1056b7c
nirmal <nirmalps@iitk.ac.in>
2019-06-11 17:43:31 +0530

    final version

--- data_loader.py
+++ data_loader.py
@@ -1,80 +1,59 @@
 from torch.utils import data
-from torchvision import transforms
+from torchvision import transforms as T
 from torchvision.datasets import ImageFolder
+from PIL import Image
 import os
+
 
 class CelebA(data.Dataset):
     """Dataset class for the CelebA dataset."""
 
-    def __init__(self, image_dir, resize_size, transform):
+    def __init__(self, image_dir, transform):
         """Initialize and preprocess the CelebA dataset."""
         self.image_dir = image_dir
-        self.attr_path = attr_path
-        self.selected_attrs = selected_attrs
-        self.resize_size = resize_size
         self.transform = transform
-        self.preprocess()
+        self.dataset = self.preprocess()
 
+        self.num_images = len(self.dataset)
 
     def preprocess(self):
         """Preprocess the CelebA attribute file."""
 
-            resize_size = self.resize_size
-            save_root = 'celeba_processed/'
-
-            if not os.path.isdir(save_root):
-                os.mkdir(save_root)
-            if not os.path.isdir(save_root + 'celebA'):
-                os.mkdir(save_root + 'celebA')
-
-            try:
-                img_list = os.listdir(self.image_dir)
-            except:
-                print('image_dir directory is invalid (if images are present inside folder /celeba/processed/ then specifiy image_dir as /celeba/)')
-                return
-
-            for i in range(len(img_list)):
-                img = plt.imread(root + img_list[i])
-                img = cv2.resize(img, (resize_size,resize_size))
-                plt.imsave(fname=save_root + 'celebA/' + img_list[i], arr=img)
-
-                if (i % 1000) == 0:
-                    print('%d images complete' % i)
-            print('data preprocessing done!')
-
-            dataset = datasets.ImageFolder(data_dir, self.transform)
-            return dataset
-
+        dataset = ImageFolder(data_dir, self.transform)
         print('Finished preprocessing the CelebA dataset...')
+        return Dataset
+
 
     def __getitem__(self, index):
-        """Return one image (Yet to inplement)."""
-        return None
+        """Return input and label for its corresponding index."""
+        data = self.dataset[index]
+        return data
+
 
     def __len__(self):
-        """Return the number of images (Yet to implement)."""
-        return None
+        """Return the number of images."""
+        return self.num_images
 
 
-def get_loader(image_dir, resize_size,
-               batch_size=128, dataset='CelebA', num_workers=1):
+def get_loader(image_dir, crop_size=178, image_size=64,
+               batch_size=64, dataset='CelebA', shuffle = True, num_workers=1):
     """Build and return a data loader."""
     transform = []
-    if mode == 'train':
-        transform = transforms.Compose([
-                transforms.ToTensor(),
-                transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
-        ])
 
+    # transform.append(T.RandomHorizontalFlip())
+    transform.append(T.CenterCrop(crop_size))
+    transform.append(T.Resize(image_size))
+    transform.append(T.ToTensor())
+    transform.append(T.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)))
+    transform = T.Compose(transform)
 
     if dataset == 'CelebA':
-        dataset = CelebA(image_dir, resize_size, transform)
+        dataset = CelebA(image_dir, transform)
     else:
-        print('specified dataset not present')
-        return None
+        print('available dataset argument availalble: celebA ')
 
     data_loader = data.DataLoader(dataset=dataset,
                                   batch_size=batch_size,
-                                  shuffle=True,
+                                  shuffle = shuffle,
                                   num_workers=num_workers)
     return data_loader
